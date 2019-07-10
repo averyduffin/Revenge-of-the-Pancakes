8258af9b228e5fada66c777d153183d3656e1d83
Avery Duffin <avery.duffin@rainfoucus.com>
2019-07-09 23:03:47 -0600

    Adding completed project. Without node modules

---
+++ index.js
@@ -0,0 +1,51 @@
+import fs from 'fs'
+import readline from 'readline'
+
+import path from 'path'
+
+const outFilePath = path.join(__dirname, `output/${process.argv[2]}.output`)
+
+fs.unlink(outFilePath, () => {
+    const readInterface = readline.createInterface({  
+        input: fs.createReadStream(path.join(__dirname, `test-cases/${process.argv[2]}.in`)),
+        console: false
+    });
+
+    let totalCases = 0
+    let caseNumber = 0
+    let totalData = ''
+
+    const calculateFlips = (pancakes) => {
+        let flipCounter = 0
+        let prev = pancakes[0]
+        for(let letter of pancakes) {
+            if(prev !== letter) {
+                flipCounter++;
+                prev = letter;
+            }
+        }
+        if(pancakes[pancakes.length -1] === '-') flipCounter++;
+        return flipCounter;
+    }
+
+    readInterface.on('line', (line) => {
+        if(caseNumber === 0) {
+            totalCases = Number(line);
+            if(!totalCases || totalCases < 0 ) throw new Error("Invalid File Format"); // just in case the file is in an incorrect format
+        }
+        else if(caseNumber <= totalCases) { // in the rare chance that there are more cases then specified
+            const data = `Case #${caseNumber}: ${calculateFlips(line)}`
+            console.log(data);
+            totalData = `${totalData}${data}\n`
+        }
+        caseNumber++;
+    });
+
+    readInterface.on('close', () => {
+        fs.appendFile(outFilePath, totalData, (err) => {
+            if (err) {
+                return console.log('Unable to write to file');
+            }
+        })
+    });
+});
